49ae0f8dbe778e906f76a42e73b295abd4d25cd3
Zesheng Chen <chenz@pfw.edu>
2022-11-18 11:04:16 -0500

    Show success or error messages

--- app/controllers/posts.controller.js
+++ app/controllers/posts.controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const { check, validationResult } = require('express-validator')
 
 module.exports = {
     showPosts: showPosts,
@@ -34,7 +35,11 @@ function showSingle(req, res) {
             res.status(404)
             res.send('Post not found!')
         }
-        res.render('pages/single', { post: post })
+
+        res.render('pages/single', {
+            post: post,
+            success: req.flash('success')
+        })
     })
 }
 
@@ -65,13 +70,26 @@ function seedPosts(req, res) {
  * Show the create form
  */
 function showCreate(req, res) {
-    res.render('pages/create')
+    res.render('pages/create', {
+        errors: req.flash('errors')
+    })
 }
 
 /**
  * Process the creation form
  */
-function processCreate(req, res) {
+async function processCreate(req, res) {
+    // validate information
+    await check('name', 'Name is required').notEmpty().run(req)
+    await check('description', 'Description is required').notEmpty().run(req)
+
+    // if there are errors, redirect and save errors to flash
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        req.flash('errors', errors.errors.map(err => err.msg))
+        return res.redirect('/posts/create')
+    }
+
     // create a new post
     const post = new Post({
         name: req.body.name,
@@ -82,6 +100,9 @@ function processCreate(req, res) {
     post.save((err) => {
         if (err)
             throw err
+
+        // set a successful flash message
+        req.flash('success', 'Successfuly created post!')
 
         // redirect to the newly created post
         res.redirect(`/posts/${post.slug}`)
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express'),
     expressLayouts = require('express-ejs-layouts'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    flash = require('connect-flash'),
     app = express(),
     port = process.env.PORT || 8080
 
@@ -22,6 +25,16 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 // use body parser to grab info from a form
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// set sessions and cookie parser
+app.use(cookieParser())
+app.use(session({
+    secret: process.env.SECRET,
+    cookie: { maxAge: 60000 },
+    resave: false, // forces the session to be saved back to the store
+    saveUninitialized: false // dont save unmodified
+}))
+app.use(flash())
 
 // set the routes
 app.use(require('./app/routes'))
