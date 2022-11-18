6d05494ffc7a6e863418e8109026d4ef64d607a8
Zesheng Chen <chenz@pfw.edu>
2022-11-18 10:19:37 -0500

    Refactor posts controller

--- app/controllers/posts.controller.js
+++ app/controllers/posts.controller.js
@@ -1,41 +1,48 @@
 const Post = require('../models/post')
 
 module.exports = {
+    showPosts: showPosts,
+    showSingle: showSingle,
+    seedPosts: seedPosts
+}
 
-    // show all posts
-    showPosts: (req, res) => {
-        // get all posts
+/** 
+ * show all posts
+ */
+function showPosts(req, res) {
+    // return a view with data
+    res.render('pages/posts', { posts: posts })
+}
 
-        // return a view with data
-        res.render('pages/posts', { posts: posts })
-    },
+/**
+ * show a single post
+ */
+function showSingle(req, res) {
+    // get a single event
+    const post = { name: 'Homework', slug: 'homework', description: 'Homework is challenging!' }
 
-    // show a single post
-    showSingle: (req, res) => {
-        // get a single post
-        const post = { name: 'Homework', slug: 'homework', description: 'Homework is challenging!' }
+    res.render('pages/single', { post: post })
+}
 
-        res.render('pages/single', { post: post })
-    },
+/**
+ * seed our database
+ */
+function seedPosts(req, res) {
+    // create some posts 
+    const posts = [
+        { name: 'Homework', description: 'Homework is challenging!' },
+        { name: 'Lecture', description: 'Lecture is fun!' },
+        { name: 'Project', description: 'Course Project is great!' }
+    ]
 
-    // seed our database
-    seedPosts: (req, res) => {
-        // create some posts 
-        const posts = [
-            { name: 'Homework', description: 'Homework is challenging!' },
-            { name: 'Lecture', description: 'Lecture is fun!' },
-            { name: 'Project', description: 'Course Project is great!' }
-        ]
+    // use the Post model to insert/save
+    Post.deleteMany({}, () => {
+        for (let post of posts) {
+            let newPost = new Post(post)
+            newPost.save()
+        }
+    })
 
-        // use the Post model to insert/save
-        Post.deleteMany({}, () => {
-            for (let post of posts) {
-                let newPost = new Post(post)
-                newPost.save()
-            }
-        })
-
-        // seeded!
-        res.send('Database seeded!')
-    }
+    // seeded!
+    res.send('Database seeded!')
 }
